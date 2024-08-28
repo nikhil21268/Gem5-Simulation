837e5730e6a451d07a4199904510cd681f4b6b02
Nikhil Suri <nikhil21268@iiitd.ac.in>
2024-08-28 13:07:37 +0530

    made some changes

--- scriptFinal.py
+++ scriptFinal.py
@@ -2,11 +2,11 @@ import m5
 from m5.objects import *
 from m5.util import addToPath
 
-# addToPath('../../')
+addToPath('../../')
 
 from common import CacheConfig
 from common.Caches import *
-from common import Platforms
+# from common import Platforms
 
 def setup_system(cpu_type):
     # Setup based on the specified CPU type
@@ -26,11 +26,11 @@ def setup_system(cpu_type):
         system.cpu = RiscvO3CPU()
 
     # Cache setup
-    system.cpu.icache = L1ICache(size='16kB', assoc=2)
+    system.cpu.icache = L1_ICache(size='16kB')
     system.cpu.icache.response_latency = 2
     system.cpu.icache.cpu_side = system.cpu.icache_port
 
-    system.cpu.dcache = L1DCache(size='16kB', assoc=2)
+    system.cpu.dcache = L1_DCache(size='16kB')
     system.cpu.dcache.response_latency = 2
     system.cpu.dcache.cpu_side = system.cpu.dcache_port
 
@@ -46,9 +46,24 @@ def setup_system(cpu_type):
     system.l2cache.mem_side = system.membus.slave
     system.system_port = system.membus.slave
 
+    # Setup interrupt controllers for each CPU thread
+    # if hasattr(system.cpu, 'createInterruptController'):
+    system.cpu.createInterruptController()
+        
     # SE mode specific setup
+
+    system.mem_ctrl = MemCtrl()
+    system.mem_ctrl.dram = DDR3_1600_8x8()
+    system.mem_ctrl.dram.range = system.mem_ranges[0]
+    system.mem_ctrl.port = system.membus.mem_side_ports
+
+
+    binary = '/home/nikhil/mibench/automotive/qsort/qsort_small.elf'
+    
+    system.workload = SEWorkload.init_compatible(binary)
+
     process = Process()
-    process.cmd = ['mibench/automotive/qsort/qsort_small.elf']
+    process.cmd = ['/home/nikhil/mibench/automotive/qsort/qsort_small.elf', '/home/nikhil/mibench/automotive/qsort/input_small.dat']
     system.cpu.workload = process
     system.cpu.createThreads()
 
@@ -64,14 +79,14 @@ def simulate_system(cpu_type):
     print('Exiting @ tick %i because %s' % (m5.curTick(), exit_event.getCause()))
 
     # Dump the statistics to a file
-    stats_file = f"stats_{cpu_type}.txt"
+    # stats_file = f"stats_{cpu_type}.txt"
     m5.stats.dump()
-    m5.stats.reset()  # Reset statistics for the next run (if any)
-    with open(stats_file, 'w') as f:
-        f.write(m5.stats.text())
-    print(f"Statistics saved to {stats_file}")
+    # m5.stats.reset()  # Reset statistics for the next run (if any)
+    # with open(stats_file, 'w') as f:
+    #    f.write(m5.stats.text())
+    # print(f"Statistics saved to {stats_file}")
 
 
-if __name__ == "__main__":
-    simulate_system("simple")  # Run with simple CPU
-    # simulate_system("o3")      # Run with O3 CPU
+print("helloworld")
+simulate_system("simple")  # Run with simple CPU
+# simulate_system("o3")      # Run with O3 CPU
